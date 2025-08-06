e2fa56b0a08773e9cb94a63997b5e5e5876e4ac1
Chester <w.chester@outlook.com>
2025-08-06 01:41:53 -0700

    Started making login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,32 @@
-import { Route, Routes } from 'react-router'
+import { Route, Routes, useLocation } from 'react-router'
 
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 
 function App() {
-  return <>
-    <NavBar />
+  const location = useLocation();
+
+  // List of routes where NavBar and Footer should be hidden
+  const hideLayoutRoutes = ['/login'];
+
+  const shouldHideLayout = hideLayoutRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {!shouldHideLayout && <NavBar />}
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />}/>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          
         </Routes>
       </main>
-    <Footer />
-  </>
+      {!shouldHideLayout && <Footer />}
+    </>
+  );
 }
 
 export default App
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,5 @@
+import '../css/Login.css'
+
+function Login() {
+    
+}
